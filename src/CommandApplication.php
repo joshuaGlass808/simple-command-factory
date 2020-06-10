9395ffb0b5fca2a20be92f5e113229c196acc8a3
josh <joshua.lee.glass@gmail.com>
2020-06-09 20:31:54 -0700

    Adding in unit tests / Configs

--- src/CommandApplication.php
+++ src/CommandApplication.php
@@ -4,14 +4,18 @@ namespace SCF;
 
 class CommandApplication
 {
-    protected array $args = [];
+    public array $args = [];
+    public array $env = [];
+    public array $config = [];
 
     /**
      * Build Application.
      */
-    public function __construct(array $args = [])
+    public function __construct(array $args = [], array $env = [], array $config = [])
     {
         $this->args = $args;
+        $this->env = $env;
+        $this->config = $config;
     }
 
     /**
@@ -19,8 +23,9 @@ class CommandApplication
      * 
      * @param bool $logRuns - If this is in production and running on cronjobs / queuing system,
      *   you may want to know when they ran, if so make $logRuns True.
+     * @return bool
      */
-    public function run(bool $logRuns = false): void 
+    public function run(bool $logRuns = false): bool
     {
         $command = array_shift($this->args);
         if (preg_match('/^(--help|-h)$/i', $command)) {
@@ -29,9 +34,8 @@ class CommandApplication
             exit(0);
         }
 
-        $args = (!empty($this->args)) ? $this->args : null;
-        
-        $class = \App\Kernel::getCommandClass($command, $args);
+        $args = (!empty($this->args)) ? $this->args : null;    
+        $class = \App\Kernel::getCommandClass($command, $args, $this->env, $this->config);
         if ($class === null) {
             $error = sprintf(
                 "The command: %s is not a valid command listed in the %s class or a valid option.\n", 
@@ -39,11 +43,11 @@ class CommandApplication
                 \App\Kernel::class
             );
             (new \SCF\Shell\BaseCmd())->error($error, true);
-
-
             exit(3);
         }
         
         $class->execute();
+
+        return true;
     }
 }
